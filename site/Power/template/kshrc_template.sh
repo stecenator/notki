fbf82eb33764dec1c74336555a0757adf5de2ebe
Marcin Steć <stecenator@gmail.com>
2025-12-31 00:16:12 +0100

    Kosmetyka README i wykluczenie site/ ze śledzenia

--- site/Power/template/kshrc_template.sh
+++
@@ -1,9 +0,0 @@
-HOST=`hostname`
-PS1="[1;31m$USER[0;31m@[0m$HOST[0;36m:\$PWD [0m# "
-PATH=/usr/es/sbin/cluster:/usr/es/sbin/cluster/utilities:/usr/ios/cli:/opt/ibm/dscli:$PATH
-HISTSIZE=5000
-EXTENDED_HISTORY=ON
-export PS1 PATH HISTSIZE EXTENDED_HISTORY 
-eval $(/usr/bin/X11/resize)
-alias rs='eval $(/usr/bin/X11/resize)'
-set -o vi
